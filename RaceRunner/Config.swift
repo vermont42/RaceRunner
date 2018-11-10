d2ba48703f6f776f62afae6c38beba40e67ffbbb
vermont42 <vermontcoder@gmail.com>
2018-11-10 09:50:18 -0800

    maintenance

--- RaceRunner/Config.swift
+++ RaceRunner/Config.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 struct Config {
-  static let darkSkyKey = "51e9be2653fc075c805c92db32a50432"
-  static let googleMapsKey = "AIzaSyCYphUv_GzYX-OIWqn77u6Cpa51e1rJ2Rs"
-  static let pubNubPublishKey = "pub-c-0d826075-d0b1-4788-a7b6-40cdffc0414d"
-  static let pubNubSubscribeKey = "sub-c-8da5fcec-8aee-11e5-a04a-0619f8945a4f"
+  static let darkSkyKey = ""
+  static let googleMapsKey = ""
+  static let pubNubPublishKey = ""
+  static let pubNubSubscribeKey = ""
   private static let googleMapsError = "Google Maps API key missing from Config.swift."
   private static let darkSkyError = "Dark Sky API key missing from Config.swift."
   private static let pubNubPublishError = "PubNub Publish API key missing from Config.swift."
